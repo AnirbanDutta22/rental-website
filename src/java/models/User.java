9aea5c2e18e39098a0dceb85958f848d8f268d18
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-27 14:52:02 +0530

    basic user model

---
+++ src/java/models/User.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author HP
+ */
+public class User {
+    private String name,username,email,password,address;
+    private int phno;
+
+    public User(String name, String username, String email, String password, String address, int phno) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.phno = phno;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhno() {
+        return phno;
+    }
+
+    public void setPhno(int phno) {
+        this.phno = phno;
+    }
+}
