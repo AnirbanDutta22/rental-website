4b002e566b7aae33a69929a059f7337ddecbdfb5
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-30 00:07:35 +0530

    Logout servlet added

---
+++ src/java/controllers/LogoutServlet.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+        // Checking if there is a session
+        if (session != null) {
+            session.invalidate(); // Invalidate the session to log the user out
+        }
+        
+        response.sendRedirect("/pages/login.jsp");
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
