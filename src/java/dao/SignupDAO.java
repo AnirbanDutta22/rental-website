36ed7243ae5accafe7d1f9ae96a4ad160c939167
Anirban Dutta <meanirbandutta22@gmail.com>
2024-11-06 13:49:49 +0530

    phone number removed

--- src/java/dao/SignupDAO.java
+++ src/java/dao/SignupDAO.java
@@ -14,11 +14,9 @@ public class SignupDAO {
 
         try (OracleConnection oconn = DBConnect.getConnection()) {
             // CHECKING IF USER ALREADY EXISTS
-            String checkUserQuery = "SELECT COUNT(*) FROM USER1 WHERE EMAIL = ? OR PHONE_NO = ? OR USERNAME = ?";
+            String checkUserQuery = "SELECT COUNT(*) FROM USER1 WHERE EMAIL = ?";
             try (OraclePreparedStatement checkStmt = (OraclePreparedStatement) oconn.prepareStatement(checkUserQuery)) {
                 checkStmt.setString(1, user.getEmail());
-                checkStmt.setLong(2, user.getPhno());
-                checkStmt.setString(3, user.getUsername());
                 try (ResultSet rs = checkStmt.executeQuery()) {
                     if (rs.next() && rs.getInt(1) > 0) {
                         return new ResponseHandler(false, "User with these credentials already exists !");
@@ -27,15 +25,14 @@ public class SignupDAO {
             }
 
             // INSERTING USER DATA TO DATABASE
-            String query = "INSERT INTO USER1 (USER_ID, NAME, EMAIL, PHONE_NO, PASSWORD, AGREEMENT, USERNAME) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            String query = "INSERT INTO USER1 (USER_ID, NAME, EMAIL, PASSWORD, AGREEMENT, USERNAME) VALUES (?, ?, ?, ?, ?, ?)";
             try (OraclePreparedStatement ops = (OraclePreparedStatement) oconn.prepareCall(query)) {
-                ops.setInt(1, 20);
+                ops.setInt(1, 15);
                 ops.setString(2, user.getName());
                 ops.setString(3, user.getEmail());
-                ops.setLong(4, user.getPhno());
-                ops.setString(5, user.getPassword());
-                ops.setString(6, user.getAgreement());
-                ops.setString(7, user.getUsername());
+                ops.setString(4, user.getPassword());
+                ops.setString(5, user.getAgreement());
+                ops.setString(6, user.getUsername());
 
                 int rowsInserted = ops.executeUpdate();
                 if (rowsInserted > 0) {
