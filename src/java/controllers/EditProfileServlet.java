5ca3a3319e1c2dcb8551e32a1848b92aae4a4d38
Anirban Dutta <meanirbandutta22@gmail.com>
2024-12-10 00:31:59 +0530

    refactor - edit address

--- src/java/controllers/EditProfileServlet.java
+++ src/java/controllers/EditProfileServlet.java
@@ -24,8 +24,10 @@ import responses.ResponseHandler;
 @MultipartConfig
 public class EditProfileServlet extends HttpServlet {
 
-    String name, email, phone, address;
+    String name, email, phone, address, district, state, pinString;
+    int pin;
     long phno;
+    boolean isVerified;
 
     // DECLARING ORACLE OBJECTS
     OracleConnection oconn;
@@ -38,23 +40,29 @@ public class EditProfileServlet extends HttpServlet {
             email = request.getParameter("email");
             phone = request.getParameter("phone");
             address = request.getParameter("address");
-            out.println(name + " " + email + " " + phone + " " + address);
+            district = request.getParameter("district");
+            state = request.getParameter("state");
+            pinString = request.getParameter("pin");
+            out.println(name + " " + email + " " + phone + " " + address + " " + district + " " + state + " " + pinString);
             Part avatar_image = request.getPart("avatar");
+            Part cover_image = request.getPart("cover");
             out.println(avatar_image);
+            out.println(cover_image);
 
             //fetching currenr user's username
             HttpSession session = request.getSession();
             User curruser = (User) session.getAttribute("user");
             String username = curruser.getUsername();
+            int id = curruser.getId();
             out.println("username" + username);
 
             //parsing phone number to long
             phno = Long.parseLong(phone);
             out.println(phno);
-
-            // updating user data
-            User user;
-
+            //parsing pin to int
+            pin = Integer.parseInt(pinString);
+            out.println(pin);
+            
             // CHECKING IF ANY FIELD IS EMPTY
             if (email == null || email.isEmpty()) {
                 email = curruser.getEmail();
@@ -65,18 +73,23 @@ public class EditProfileServlet extends HttpServlet {
             if (phone == null || phone.isEmpty()) {
                 phno = curruser.getPhno();
             }
-
-            user = new User(name, email, phno, address, username);
+            
+            //CHECKING IF ADDRESS, DISTRICT, STATE, PIN IS EMPTY OR NOT
+            isVerified = !(address.isEmpty() || address == null || district.isEmpty() || district == null || state.isEmpty() || state == null || pinString.isEmpty() || pinString == null);
+            out.println(isVerified);
+            // updating user data
+            User user;
+            user = new User(id, name, email, phno, address, district, state, pin,isVerified, username);
             out.println(user);
 
             //avatar image upload
-            if (avatar_image != null && avatar_image.getSize() > 0) {
+            if ((avatar_image != null && avatar_image.getSize() > 0) || (cover_image != null && cover_image.getSize() > 0)) {
                 // Defining the upload path
                 String uploadDir = "uploads";
                 String avatarFileName = username + "_avatar" + getFileExtension(avatar_image.getSubmittedFileName());
+                String coverFileName = username + "_cover" + getFileExtension(cover_image.getSubmittedFileName());
 
                 // Ensuring the upload directory exists
-//                String uploadPath = "C:\\Users\\ratna\\OneDrive\\Documents\\Desktop\\rental-website\\web\\" + uploadDir;
                 String uploadPath = getServletContext().getRealPath("") + File.separator + uploadDir;
                 out.println("uploads path : " + uploadPath);
                 File uploadDirectory = new File(uploadPath);
@@ -85,23 +98,30 @@ public class EditProfileServlet extends HttpServlet {
                 }
 
                 // Full path to save the file
-                String fullPath = uploadPath + File.separator + avatarFileName;
+                String fullAvatarPath = uploadPath + File.separator + avatarFileName;
+                String fullCoverPath = uploadPath + File.separator + coverFileName;
                 try {
-                    avatar_image.write(fullPath);
-                    out.println("File written successfully to: " + fullPath);
+                    avatar_image.write(fullAvatarPath);
+                    out.println("File written successfully to: " + fullAvatarPath);
+                    cover_image.write(fullCoverPath);
+                    out.println("File written successfully to: " + fullCoverPath);
                 } catch (IOException e) {
                     e.printStackTrace();
                     out.println("File write failed: " + e.getMessage());
                 }
-                out.println("Full path : " + fullPath);
+                out.println("Full cover path : " + fullCoverPath + "Full avatar path : " + fullAvatarPath);
 
                 String avatarPath = uploadDir + "/" + avatarFileName;
+                String coverPath = uploadDir + "/" + coverFileName;
                 user.setAvatar_image(avatarPath);
-                out.println("path for db : " + user.getAvatar_image());
+                user.setCover_image(coverPath);
+                out.println("path for db : " + user.getAvatar_image() + " and " + user.getCover_image());
             } else {
                 user.setAvatar_image(curruser.getAvatar_image());
+                user.setCover_image(curruser.getCover_image());
             }
             out.println(user.getAvatar_image());
+            out.println(user.getCover_image());
 
             // USING userDAO TO update/edit USER
             UserDAO userDAO = new UserDAO();
